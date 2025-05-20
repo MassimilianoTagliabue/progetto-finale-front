89a945e8b92497477703466cfcd2c4318ecd8215
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-05-20 23:20:18 +0200

    feat: searchbar + pagina card dettagliata

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,7 @@ function Header() {
             <nav className="navbar bg-dark p-3 "  data-bs-theme="dark">
                 <div className="container-fluid mx-3">
                     <NavLink className="navbar-brand" to="/">Videogame</NavLink>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    
                 </div>
             </nav>
         </>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 import AppCard from "../components/AppCard";
 import { Link } from "react-router-dom";
@@ -7,30 +7,45 @@ import VideogameDetail from "./VideogameDetail";
 function Homepage() {
     const globalProviderValue = useContext(GlobalContext);
     const { videogame, setVideogame } = globalProviderValue;
+    const [search, setSearch] = useState("")
+
+    // Filtra i videogiochi in base alla ricerca
+    const filter = videogame.filter((game) =>
+    game.title.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <>
+            <div className="mt-3 d-flex justify-content-end">
+                <form className="d-flex col-4 mx-4" data-bs-theme="dark" role="search" onSubmit={(e) => e.preventDefault()} >
+                    <input className="form-control me-2" type="text" name="title" placeholder="Search" aria-label="Search"
+                        autoComplete="off" value={search} onChange={(event) => setSearch(event.target.value)}/>
+                    <button className="btn btn-success" type="submit">Search</button>
+                </form>
+            </div>
+
             <div className="col-10 mx-auto flex-wrap mt-5 d-flex justify-content-between">
-                {videogame.length > 0 ? (
+
+                {filter.length > 0 ? (
                     <>
 
-                    {videogame.map((curVideogame) =>
-                        <div key={curVideogame.id} className="mb-5" >
-                            
-                            <AppCard
-                            
-                            videogame={curVideogame}
-                            />
-                           
-                          
+                        {filter.map((curVideogame) =>
+                            <div key={curVideogame.id} className="mb-5" >
+
+                                <AppCard
+
+                                    videogame={curVideogame}
+                                />
+
+
+                            </div>
+
+                        )}
+                        <div className="col-3 " >
+
+
                         </div>
 
-                    )}
-                    <div className="col-3 " >
-                        
-                            
-                    </div>
-                    
                     </>
 
                 ) : (
